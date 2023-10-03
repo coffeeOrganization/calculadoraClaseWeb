ab623d196887ad5da387d8b055b98c54daf34fa7
Mauricio Cruz <mauriciogonzalolopezcruz@gmail.com>
2023-10-02 22:30:05 -0300

    puede ingresar los todos los valores en el input

---
+++ javascript/javascript.js
@@ -0,0 +1,35 @@
+const input = document.getElementById("input");
+
+let primerNumero="";
+let segundoNumero="";
+let operador="";
+function probando(valor){
+    input.value += valor;
+}
+
+function mas(){
+    primerNumero=input.value;
+    input.value += "+";
+    operador="+";
+}
+function menos(){
+    input.value += "-";
+    operador="-";
+}
+function multiplicar(){
+    input.value += "*";
+    operador="*";
+}
+function dividir(){
+    input.value += "/";
+    operador="/";
+}
+
+function calcular(){
+    segundoNumero=input.value;
+    cantidadCaracter = primerNumero.length + 1;
+    cantidadCaracterSegundo = segundoNumero.length;
+    segundoNumero = segundoNumero.slice(cantidadCaracter, cantidadCaracterSegundo);
+    alert(segundoNumero);
+}
+
